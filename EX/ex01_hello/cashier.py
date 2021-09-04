076e3f34411526f92ddf69474ccd072f136ac20c
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-09-04 18:37:18 +0300

    hello-v15

---
+++ EX/ex01_hello/cashier.py
@@ -0,0 +1,14 @@
+"""Cha-ching!"""
+
+amount = int(input("Enter a sum: "))
+coins = 0
+
+mod_50 = amount % 50
+coins += (amount - mod_50)/50
+mod_20 = mod_50 % 20
+coins += (mod_50 - mod_20)/20
+mod_10 = mod_20 % 10
+coins += (mod_20 - mod_10)/10
+mod_5 = mod_10 % 5
+coins += (mod_10 - mod_5)/5 + mod_5
+print(f"Amount of coins needed: {coins}")
