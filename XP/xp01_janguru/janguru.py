229f2eef4b4c029f015af98797181f82a8d95fd4
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-09-05 17:49:33 +0300

    Janguru-v3

--- XP/xp01_janguru/janguru.py
+++ XP/xp01_janguru/janguru.py
@@ -1,3 +1,6 @@
+import math
+
+
 def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2) -> int:
     """Calculate the meeting position of 2 jangurus.
     @:param pos1: position of first janguru
@@ -15,16 +18,39 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2) -> int:
         return -1
     if (pos2 < pos1) and (speed2 <= speed1):
         return -1
+    if (pos1 < pos2) and (speed1 > speed2):
+        time = 0
+        pos1_sleep = 1
+        pos2_sleep = 1
+        while pos1 <= pos2 + jump_distance2:
+            if pos1 == pos2:
+                return pos1
+            pos1_sleep -= 1
+            pos2_sleep -= 1
+            if pos1_sleep <= 0:
+                pos1 += jump_distance1
+                pos1_sleep = sleep1
+            if pos2_sleep <= 0:
+                pos2 += jump_distance2
+                pos2_sleep = sleep2
+
+
+            time += 1
+    if (pos2 < pos1) and (speed2 > speed1):
+        time = 0
+        pos2_sleep = 1
+        pos1_sleep = 1
+        while pos2 <= pos1 + jump_distance1:
+            if pos1 == pos2:
+                return pos1
+            pos2_sleep -= 1
+            pos1_sleep -= 1
+            if pos2_sleep <= 0:
+                pos2 += jump_distance2
+                pos2_sleep = sleep2
+            if pos1_sleep <= 0:
+                pos1 += jump_distance1
+                pos1_sleep = sleep1
+            time += 1
     else:
-        if (pos2 > pos1) and (speed1 > speed2):
-            relative_distance = pos2 - pos1
-            relative_speed = speed1 - speed2
-            time = relative_distance / relative_speed
-            return int(pos1 + speed1 * time)
-        if (pos1 > pos2) and (speed2 > speed1):
-            relative_distance = pos1 - pos2
-            relative_speed = speed2 - speed1
-            time = relative_distance / relative_speed
-            return int(pos2 + speed2 * time)
-
-
+        return -1
