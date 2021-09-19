ff7b0d1e6b5d49c8a425a356cddba31188ea9465
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-09-19 20:22:04 +0300

    id-code_pre_generation

--- EX/ex03_idcode/idcode.py
+++ EX/ex03_idcode/idcode.py
@@ -236,14 +236,15 @@ def is_id_valid(id_code: str) -> bool:
     :param id_code: str
     :return: boolean
     """
-    if find_id_code(id_code):
-        if is_valid_gender_number(int(id_code[0])):
-            if is_valid_year_number(int(id_code[1:3])):
-                if is_valid_month_number(int(id_code[3:5])):
-                    if is_valid_day_number(int(id_code[0]), int(id_code[1:3]), int(id_code[3:5]), int(id_code[5:7])):
-                        if is_valid_birth_number(int(id_code[7:10])):
-                            if is_valid_control_number(id_code):
-                                return True
+    if len(id_code) != 11:
+        return False
+    if is_valid_gender_number(int(id_code[0])):
+        if is_valid_year_number(int(id_code[1:3])):
+            if is_valid_month_number(int(id_code[3:5])):
+                if is_valid_day_number(int(id_code[0]), int(id_code[1:3]), int(id_code[3:5]), int(id_code[5:7])):
+                    if is_valid_birth_number(int(id_code[7:10])):
+                        if is_valid_control_number(id_code):
+                            return True
     return False
 
 
