ebf48cea74563a03d0b9951e4cc3c664f6b02dd7
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-09-20 18:19:18 +0300

    lists

--- EX/ex04_lists/lists.py
+++ EX/ex04_lists/lists.py
@@ -1,11 +1,35 @@
 def generate_list(amount: int, data_type: str) -> list:
     """Write a function that returns a list with amount elements of type data_type."""
     a = []
-    b = type(data_type)
-    print(b)
-    for i in range(amount):
-        a.append(b)
+    if data_type == "int":
+        b = int
+        for i in range(amount):
+            a.append(b)
+    if data_type == "float":
+        b = float
+        for i in range(amount):
+            a.append(b)
+    if data_type == "string":
+        b = str
+        for i in range(amount):
+            a.append(b)
+    if data_type == "list":
+        b = list
+        for i in range(amount):
+            a.append(b)
+    if data_type == "tuple":
+        b = tuple
+        for i in range(amount):
+            a.append(b)
+    if data_type == "dict":
+        b = {}
+        for i in range(amount):
+            a.append(b)
+    if data_type == "set":
+        b = set()
+        for i in range(amount):
+            a.append(b)
     return a
 
 
-print(generate_list(5, 5))
+print(generate_list(5, "dict"))
