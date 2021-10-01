3b42f4da216ef635e7ca186c314c6c876215fbbd
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-10-01 12:50:23 +0300

    Examv1

---
+++ TK/tk3/exam.py
@@ -0,0 +1,100 @@
+"""TK 3."""
+
+
+def make_ends(nums: list) -> list:
+    """
+    Given an array of ints, return a new array length 2 containing the first and last elements from the original array.
+
+    The original array will be length 1 or more.
+
+    make_ends([1, 2, 3]) → [1, 3]
+    make_ends([1, 2, 3, 4]) → [1, 4]
+    make_ends([7, 4, 6, 2]) → [7, 2]
+
+    :param nums: List of integers.
+    :return: List with the first and the last element from the input list.
+    """
+    new_list = [nums[0], nums[-1]]
+    return new_list
+
+
+def is_sum_of_two(a: int, b: int, c: int) -> bool:
+    """
+    Whether one parameter is a sum of other two.
+
+    is_sum_of_two(3, 2, 1) => True
+    is_sum_of_two(3, 1, 1) => False
+    is_sum_of_two(3, 2, 5) => True
+    """
+    all_values = sorted([a, b, c])
+    if all_values[0] + all_values[1] == all_values[2]:
+        return True
+    else:
+        return False
+
+
+def first_half(text: str) -> str:
+    """
+    Return the first half of an string.
+
+    The length of the string is even.
+
+    first_half('HaaHoo') => 'Haa'
+    first_half('HelloThere') => 'Hello'
+    first_half('abcdef') => 'abc'
+    """
+    half_text_length = int(len(text) / 2)
+    half_text = text[0: half_text_length]
+    return half_text
+
+
+def non_decreasing_list(nums: list) -> bool:
+    """
+    Given a list of numbers.
+
+    If given list is a non-decreasing list, return True, otherwise False.
+    Non-decreasing means every next element in the list must not be smaller than the previous one.
+
+    non_decreasing_list([0, 1, 2, 3, 98]) => True
+    non_decreasing_list([50, 49]) => False
+    non_decreasing_list([12, 12]) => True
+
+    :param nums:
+    :return:
+    """
+    sorted_nums = sorted(nums)
+    sorted_nums.reverse()
+    if sorted_nums == nums:
+        return False
+    else:
+        return True
+
+
+
+def mirror_ends(s: str) -> str:
+    """
+    Given a string, look for a mirror image (backwards) string at both the beginning and end of the given string.
+
+    In other words, zero or more characters at the very beginning of the given string,
+    and at the very end of the string in reverse order (possibly overlapping).
+
+    For example, the string "abXYZba" has the mirror end "ab".
+
+    mirrorEnds("abXYZba") → "ab"
+    mirrorEnds("abca") → "a"
+    mirrorEnds("aba") → "aba"
+
+    :param s: String
+    :return: Mirror image string
+    """
+    word_length = len(s)
+    mirror_word = ""
+    for i in range(0, word_length-1):
+        if s[i] == s[word_length - i - 1]:
+            mirror_word += s[i]
+        else:
+            return mirror_word
+    return mirror_word + s[0]
+
+
+print(non_decreasing_list([12, 12]))
