ac93d20aa3aa18e41b704a43aa596dc2ccf1d66a
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-10-02 18:51:21 +0300

    Hobbiesv2

--- EX/ex05_hobbies/hobbies.py
+++ EX/ex05_hobbies/hobbies.py
@@ -216,31 +216,3 @@ def sort_names_and_hobbies(data: str) -> tuple:
         name_hobbies_list.append(new_tuple)
     final_tuple = tuple(name_hobbies_list)
     return final_tuple
-
-
-if __name__ == '__main__':
-    sample_data = """Jack:crafting\nPeter:hiking\nWendy:gaming\nMonica:tennis\nChris:origami\nSophie:sport\nMonica:design\nCarmen:sport\nChris:sport\nMonica:skateboarding\nCarmen:cooking\nWendy:photography\nMonica:tennis\nCooper:yoga\nWendy:sport\nCooper:movies\nMonica:theatre\nCooper:yoga\nChris:gaming\nMolly:fishing\nJack:skateboarding\nWendy:fishing\nJack:drawing\nMonica:baking\nSophie:baking\nAlfred:driving\nAlfred:shopping\nAlfred:crafting\nJack:drawing\nCarmen:shopping\nCarmen:driving\nPeter:drawing\nCarmen:shopping\nWendy:fitness\nAlfred:travel\nJack:origami\nSophie:design\nJack:pets\nCarmen:dance\nAlfred:baking\nSophie:sport\nPeter:gaming\nJack:skateboarding\nCooper:football\nAlfred:sport\nCooper:fitness\nChris:yoga\nWendy:football\nMolly:design\nJack:hiking\nMonica:pets\nCarmen:photography\nJack:baking\nPeter:driving\nChris:driving\nCarmen:driving\nPeter:theatre\nMolly:hiking\nWendy:puzzles\nJack:crafting\nPeter:photography\nCarmen:theatre\nSophie:crafting\nCarmen:cooking\nAlfred:gaming\nPeter:theatre\nCooper:hiking\nChris:football\nChris:pets\nJack:football\nMonica:skateboarding\nChris:driving\nCarmen:pets\nCooper:gaming\nChris:hiking\nJack:cooking\nPeter:fishing\nJack:gaming\nPeter:origami\nCarmen:movies\nSophie:driving\nJack:sport\nCarmen:theatre\nWendy:shopping\nCarmen:pets\nWendy:gaming\nSophie:football\nWendy:theatre\nCarmen:football\nMolly:theatre\nPeter:theatre\nMonica:flowers\nMolly:skateboarding\nPeter:driving\nSophie:travel\nMonica:photography\nCooper:cooking\nJack:fitness\nPeter:cooking\nChris:gaming"""
-    dic = create_dictionary(sample_data)
-    print("shopping" in dic["Wendy"])  # -> True
-    print("fitness" in dic["Sophie"])  # -> False
-    print("gaming" in dic["Peter"])  # -> True
-    print(find_people_with_most_hobbies(sample_data))  # -> ['Jack']
-    print(len(dic["Jack"]))  # ->  12
-    print(len(dic["Carmen"]))  # -> 10
-    print(find_people_with_least_hobbies(sample_data))  # -> ['Molly']
-    print(len(dic["Molly"]))  # -> 5
-    print(len(dic["Sophie"]))  # -> 7
-    print(find_most_popular_hobbies(sample_data))  # -> ['football', 'gaming', 'sport']
-    print(find_least_popular_hobbies(sample_data))  # -> ['dance', 'flowers', 'puzzles', 'tennis']
-    print(create_dictionary_with_hobbies(sample_data))
-    sort_result = sort_names_and_hobbies(sample_data)
-    # if the condition after assert is False, error will be thrown
-    assert isinstance(sort_result, tuple)
-    assert len(sort_result) == 10
-    assert sort_result[0][0] == 'Alfred'
-    assert len(sort_result[0][1]) == 7
-    assert sort_result[-1] == ('Wendy', ('fishing', 'fitness', 'football', 'gaming', 'photography', 'puzzles', 'shopping', 'sport', 'theatre'))
-    # if you see this line below, then everything seems to be ok!
-    print("sorting works!")
-
-    # print(sort_names_and_hobbies(sample_data))
