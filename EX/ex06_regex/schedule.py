871f50b086bd22e3802a6e41909371b16b76cc7a
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-10-10 18:30:45 +0300

    regex_schedule

--- EX/ex06_regex/schedule.py
+++ EX/ex06_regex/schedule.py
@@ -46,53 +46,6 @@ def create_schedule_string(input_string: str) -> str:
     return "\n".join(table)
 
 
-def make_all_activities_single_item(converted_dictionary: dict) -> dict:
-    """
-    Join all activities into one single list as a string.
-
-    :param converted_dictionary: Dictionary with correct times and activities in it as a list
-    :return: Dictionary with all keys having a single string as values
-    """
-    for times in converted_dictionary.keys():
-        if len(converted_dictionary[times]) > 0:
-            joined_list = ", ".join(converted_dictionary[times])
-            converted_dictionary[times] = joined_list
-    return converted_dictionary
-
-
-def convert_dictionary_to_12h_format(dictionary_with_24h: dict) -> dict:
-    """
-    Convert dictionary with 24h time format to a one with 12h time formats.
-
-    :param dictionary_with_24h:
-    :return: dictionary that has 12h formats in it
-    """
-    times = list(dictionary_with_24h.keys())
-    activities = list(dictionary_with_24h.values())
-    for time in range(0, len(times)):
-        new_time = convert_to_12_hour_format(times[time])
-        times[time] = new_time
-    time_activity_dict_12h = {}
-    for time_slots in range(0, len(times)):
-        time_activity_dict_12h[times[time_slots]] = activities[time_slots]
-    return time_activity_dict_12h
-
-
-def get_table_size(schedule_dict: dict) -> list:
-    """
-    Get the needed line lengths to create a schedule table.
-
-    :param schedule_dict: Sorted dictionary of schedule times and activities.
-    :return: List of parameters to be used to create the schedule table.
-    """
-    if len(schedule_dict.keys()) == 0 or len(schedule_dict.values()) == 0:
-        return [0, 0, 0]
-
-    maximum_time_length = max(len(time) for time in schedule_dict)
-    maximum_activity_length = max(len(activity) for activity in schedule_dict.values())
-    return [maximum_time_length, maximum_activity_length, maximum_activity_length + maximum_time_length]
-
-
 def convert_to_12_hour_format(time: str) -> str:
     """
     Convert 24 hour times into 12h times.
@@ -109,33 +62,6 @@ def convert_to_12_hour_format(time: str) -> str:
     else:
         hours = str(int(hours) - 12)
         return f"{hours}:{minutes} PM"
-
-
-def create_sorted_schedule_dictionary(schedule_list: list) -> dict:
-    """
-    Create a schedule dictionary out of schedule list.
-
-    Create schedule dictionary and group together timestamps.
-    :param schedule_list: list of times and activities
-    :return: Schedule dictionary
-    """
-    time_activity_dict = {}
-    for events in schedule_list:
-        hours = int(events[0])
-        minutes = int(events[1])
-        if hours < 24 and minutes < 59:
-            timestamp = f"{str(events[0]).zfill(2)}:{str(events[1]).zfill(2)}"
-            if timestamp not in time_activity_dict.keys():
-                time_activity_dict[timestamp] = [events[2].lower()]
-            elif timestamp in time_activity_dict.keys() and events[2].lower() in time_activity_dict[timestamp]:
-                continue
-            else:
-                time_activity_dict[timestamp].append(events[2].lower())
-    keys_sorted = sorted(time_activity_dict.keys())
-    sorted_dict = {}
-    for key in keys_sorted:
-        sorted_dict[key] = time_activity_dict[key]
-    return sorted_dict
 
 
 if __name__ == '__main__':
