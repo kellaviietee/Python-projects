24cbaa348dcfa8d15ed8357c951cf488b004c1a6
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-10-19 19:37:11 +0300

    testing

--- EX/ex08_tests/solution.py
+++ EX/ex08_tests/solution.py
@@ -1,4 +1,9 @@
-"""Is this i a docstring."""
+"""
+This is an example script.
+
+It seems that it has to have THIS docstring with a summary line, a blank line
+and sume more text like here. Wow.
+"""
 
 
 def generate_list(input_amount, data_type):
