f4c5c4364d18024dc693c317ce01faa30d480674
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-10-19 19:49:43 +0300

    testing

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -34,7 +34,7 @@ def test_morning_no_coffee():
     assert result == expected_result
 
 
-def test_overtime():
+def test_overtime_no_coffee():
     coffee_needs = False
     clock = 36
     result = solution.students_study(clock, coffee_needs)
@@ -42,8 +42,23 @@ def test_overtime():
     assert result == expected_result
 
 
-def test_undertime():
+def test_overtime_yes_coffee():
+    coffee_needs = True
+    clock = 36
+    result = solution.students_study(clock, coffee_needs)
+    expected_result = False
+    assert result == expected_result
+
+
+def test_undertime_no_coffee():
     coffee_needs = False
+    clock = 36
+    result = solution.students_study(clock, coffee_needs)
+    expected_result = False
+    assert result == expected_result
+
+def test_undertime_yes_coffee():
+    coffee_needs = True
     clock = 36
     result = solution.students_study(clock, coffee_needs)
     expected_result = False
