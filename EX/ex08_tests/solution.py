400846988652c90f6de11be738c27cd4b46d9045
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-10-19 10:18:10 +0300

    file_handling

--- EX/ex08_tests/solution.py
+++ EX/ex08_tests/solution.py
@@ -30,3 +30,49 @@ def generate_list(input_amount, data_type):
             b.add(i)
             a.append(b)
     return a
+
+
+def generate_combined_list(inputs: list) -> list:
+    """
+    Write a function that returns a list with the minimal possible length, that still satisfies the criteria below.
+
+    Every element of 'inputs' is a tuple (int amount, string data_type).
+    For each element of 'inputs', it must be true that the returned list contains at least 'amount' of elements of type 'data_type'.
+    """
+    what_we_need = {}
+    for item in inputs:
+        if item[1] in what_we_need.keys():
+            if item[0] > what_we_need[item[1]]:
+                what_we_need[item[1]] = item[0]
+            else:
+                continue
+        else:
+            what_we_need[item[1]] = item[0]
+    combined_list = []
+    for key in what_we_need:
+        combined_list += (generate_list(what_we_need[key], key))
+    return combined_list
+
+
+def generate_combined_list_unique(inputs: list) -> list:
+    """
+    Write a function that returns a list with the minimal possible length, that still satisfies the criteria below.
+
+    Every element of 'inputs' is a tuple (int amount, string data_type).
+    For each element of 'inputs', it must be true that the returned list contains at least 'amount' of elements of type 'data_type'.
+    Data types used in this function are 'int', 'float' and 'str' (string).
+    The returned list can contain only unique elements.
+    """
+    what_we_need = {}
+    for item in inputs:
+        if item[1] in what_we_need.keys():
+            if item[0] > what_we_need[item[1]]:
+                what_we_need[item[1]] = item[0]
+            else:
+                continue
+        else:
+            what_we_need[item[1]] = item[0]
+    combined_list = []
+    for key in what_we_need:
+        combined_list += (generate_list(what_we_need[key], key))
+    return combined_list
