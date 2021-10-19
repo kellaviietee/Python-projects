daf4db676d2a368d39b32d97652d29d433f20bbc
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-10-19 10:22:46 +0300

    testing

--- EX/ex08_tests/test.py
+++ EX/ex08_tests/test.py
@@ -7,3 +7,10 @@ def test_part1_int_correct_len():
     res = solution.generate_list(input_amount, "string")
     expected_len = 5
     assert len(res) == expected_len
+
+
+def test_part1_int_no_len():
+    input_amount = 0
+    res = solution.generate_list(input_amount, "string")
+    expected_len = 0
+    assert len(res) == expected_len
