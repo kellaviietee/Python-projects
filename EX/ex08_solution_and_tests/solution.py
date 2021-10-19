525953ea7c0ff8c893a61c172972b93af2b33f1c
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-10-19 21:35:05 +0300

    testing

--- EX/ex08_solution_and_tests/solution.py
+++ EX/ex08_solution_and_tests/solution.py
@@ -9,11 +9,11 @@ def students_study(time: int, coffee_needed: bool) -> bool:
    :param coffee_needed: is the coffee needed
    :return: Return True if students study in given circumstances.
    """
-    if time is None or coffee_needed is None:
-        return False
-    if (18 <= time <= 24) and not coffee_needed:
+    if (18 <= time <= 24):
         return True
     elif (5 <= time <= 17) and coffee_needed:
         return True
     elif 1 <= time <= 4:
         return False
+    else:
+        return None
