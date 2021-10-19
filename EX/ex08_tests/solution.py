4baaa0bd04373b71af41f2c6098030beb9127769
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-10-19 19:38:35 +0300

    testing

--- EX/ex08_tests/solution.py
+++ EX/ex08_tests/solution.py
@@ -2,7 +2,7 @@
 This is an example script.
 
 It seems that it has to have THIS docstring with a summary line, a blank line
-and sume more text like here. Wow.
+and some more text like here. Wow.
 """
 
 
