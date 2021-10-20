9ad7e41d6f3c3fd9cf08499d392030eacec17d8a
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-10-20 20:32:51 +0300

    testing

--- EX/ex08_tests/test.py
+++ EX/ex08_tests/test.py
@@ -63,14 +63,3 @@ def test_part1_wrong_datatype():
     assert len(res) == expected_len
 
 
-def test_part1_both_wrong():
-    """
-    Test if both inputs are not inserted.
-
-    :return:
-    """
-    res = solution.generate_list(0, "")
-    expected_len = 0
-    assert len(res) == expected_len
-
-
