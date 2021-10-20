b0119e3b52a11d49f5fdc52f4ae9fbd2b3efada7
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-10-20 08:41:41 +0300

    testing

--- EX/ex08_solution_and_tests/solution.py
+++ EX/ex08_solution_and_tests/solution.py
@@ -17,3 +17,22 @@ def students_study(time: int, coffee_needed: bool) -> bool:
         return False
     else:
         return False
+
+
+def lottery(a: int, b: int, c: int) -> int:
+    """
+    Return Lottery victory result 10, 5, 1, or 0 according to input values.
+
+    (5, 5, 5) -> 10
+    (2, 2, 1) -> 0
+    (2, 3, 1) -> 1
+    """
+    if a == 5 and b == 5 and c == 5:
+        return 10
+    elif a == b and b == c:
+        return 5
+    elif a != b and a != c and b != c:
+        return 1
+    else:
+        return 0
+
--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -63,3 +63,39 @@ def test_undertime_yes_coffee():
     result = solution.students_study(clock, coffee_needs)
     expected_result = False
     assert result == expected_result
+
+
+def test_lottery_jackpot():
+    a = 5
+    b = 5
+    c = 5
+    result = solution.lottery(a, b, c)
+    expected_result = 10
+    assert result == expected_result
+
+
+def test_lottery_small_win():
+    a = 1
+    b = 1
+    c = 1
+    result = solution.lottery(a, b, c)
+    expected_result = 5
+    assert result == expected_result
+
+
+def test_lottery_constallation_win():
+    a = 5
+    b = 6
+    c = 7
+    result = solution.lottery(a, b, c)
+    expected_result = 1
+    assert result == expected_result
+
+
+def test_lottery_no_win():
+    a = 5
+    b = 6
+    c = 6
+    result = solution.lottery(a, b, c)
+    expected_result = 0
+    assert result == expected_result
