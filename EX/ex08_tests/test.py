b42fb09460bfe5123e95c5e375214546cc2996a6
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-10-21 09:26:49 +0300

    testing

--- EX/ex08_tests/test.py
+++ EX/ex08_tests/test.py
@@ -94,3 +94,9 @@ def test_generate_combined_list_correct_float():
     result = solution.generate_combined_list(input_to_list)
     expected_result = 5
     assert len(result) == expected_result
+
+def test_generate_combined_list_wrong_types():
+    input_to_list = [(5, "hmm"), (3, "wee")]
+    result = solution.generate_combined_list(input_to_list)
+    expected_result = 0
+    assert len(result) == expected_result
