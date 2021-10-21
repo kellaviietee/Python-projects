9c940445b4dfc9e78dacd3cc62ca79daed2daccd
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-10-21 13:55:32 +0300

    twitter

--- MX/mx_twitter/twitter.py
+++ MX/mx_twitter/twitter.py
@@ -75,14 +75,11 @@ def filter_by_hashtag(tweets: list, hashtag: str) -> list:
     :param hashtag: Hashtag to filter by.
     :return: Filtered list of tweets.
     """
-    hashtag_users = []
+    tweets_with_hastag = []
     for tweet in tweets:
-        match = re.findall(hashtag, tweet.content)
-        if match is None:
-            continue
-        else:
-            hashtag_users.append(tweet)
-    return hashtag_users
+        if hashtag in tweet.content:
+            tweets_with_hastag.append(tweet)
+    return tweets_with_hastag
 
 
 def sort_hashtags_by_popularity(tweets: list) -> list:
@@ -108,7 +105,6 @@ def sort_hashtags_by_popularity(tweets: list) -> list:
             else:
                 all_hashtags[hashtag] += tweet.retweets
     this_is_dummy = all_hashtags.copy()
-    print(this_is_dummy)
     hashtag_popularity = sorted(all_hashtags, key=lambda x: (this_is_dummy[x], x), reverse= True)
     return hashtag_popularity
 
@@ -130,14 +126,5 @@ if __name__ == '__main__':
     tweet3 = Tweet("@CIA", "We can neither confirm nor deny that this is our first tweet. #heart", 2192, 284200)
     tweets = [tweet1, tweet2, tweet3]
 
-    print(find_fastest_growing(tweets).user)  # -> "@elonmusk"
-    filtered_by_popularity = sort_by_popularity(tweets)
-    print(filtered_by_popularity[0].user)  # -> "@CIA"
-    print(filtered_by_popularity[1].user)  # -> "@elonmusk"
-    print(filtered_by_popularity[2].user)  # -> "@realDonaldTrump"
-    filtered_by_hashtag = filter_by_hashtag(tweets, "#bigsmart")
-    print(filtered_by_hashtag[0].user)  # -> "@realDonaldTrump"
-    print(filtered_by_hashtag[1].user)  # -> "@elonMusk"
-    print(sort_hashtags_by_popularity(tweets))
-    sorted_hashtags = sort_hashtags_by_popularity(tweets)
-    print(sorted_hashtags[0])  # -> "#heart"
+    print(filter_by_hashtag(tweets, "#heart"))
+
