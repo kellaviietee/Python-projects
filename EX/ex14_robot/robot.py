c3630dab6a2c180529d812c78347843bf5e59fc3
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-12-06 21:04:26 +0200

    robot

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -1,14 +1,8 @@
-"""Virtual roomba robot."""
 from FollowerBot import FollowerBot
 
+robot = FollowerBot()
 
-def test_run(robot: FollowerBot):
-    """See if the robot even moves at all."""
-    robot.set_wheels_speed(30)
-    robot.sleep(10)
-    robot.set_wheels_speed(0)
-    robot.done()
-
-
-if __name__ == "main":
-    pass
+robot.set_wheels_speed(30)
+robot.sleep(2)
+robot.set_wheels_speed(0)
+robot.done()
