5e14e373cc6ffbb9b4a515654cec199300f27a1d
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-12-07 14:58:24 +0200

    robot

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -3,7 +3,6 @@ from FollowerBot import FollowerBot
 
 def test_run(robot: FollowerBot):
     robot.set_wheels_speed(30)
-    robot.sleep(2)
+    robot.sleep(3)
     robot.set_wheels_speed(0)
     robot.done()
-
