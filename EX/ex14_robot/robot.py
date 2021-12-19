57fef883cb5ab27dd3fc582b41e063fd323b4d1f
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-12-19 16:53:26 +0200

    robot

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -20,6 +20,38 @@ def drive_to_line(robot: FollowerBot):
     robot.done()
 
 
+def follow_the_line(robot: FollowerBot):
+    while robot.get_line_sensors() == [1024, 1024, 1024, 1024, 1024, 1024]:
+        robot.set_wheels_speed(100)
+        robot.sleep(0.01)
+        robot.set_wheels_speed(0)
+    while 0 in robot.get_line_sensors():
+        if robot.get_left_line_sensor() == 0 and robot.get_right_line_sensor() == 0:
+            print(robot.get_line_sensors())
+            robot.set_wheels_speed(100)
+            robot.sleep(0.01)
+            robot.set_wheels_speed(0)
+        elif robot.get_left_line_sensor() == 1024 and robot.get_right_line_sensor() == 0:
+            robot.set_left_wheel_speed(-100)
+            robot.set_right_wheel_speed(100)
+            robot.sleep(0.01)
+            robot.set_wheels_speed(0)
+            print(robot.get_left_line_sensor())
+            print(robot.get_right_line_sensor())
+        elif robot.get_left_line_sensor() == 0 and robot.get_right_line_sensor() == 1024:
+            robot.set_right_wheel_speed(-100)
+            robot.set_left_wheel_speed(100)
+            robot.sleep(0.01)
+            robot.set_wheels_speed(0)
+            print(robot.get_left_line_sensor())
+            print(robot.get_right_line_sensor())
+        else:
+            break
+    print(robot.get_left_line_sensor())
+    print(robot.get_right_line_sensor())
+    robot.done()
+
+
 if __name__ == '__main__':
-    robo = FollowerBot(start_x=250, start_y=250)
-    drive_to_line(robo)
+    robo = FollowerBot(start_x=380, start_y=255)
+    follow_the_line(robo)
