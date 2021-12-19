4382516f0c793578adafd675a02310a6a22b9559
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-12-19 15:22:57 +0200

    robot

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -8,6 +8,18 @@ def test_run(robot: FollowerBot):
     robot.done()
 
 
+def drive_to_line(robot: FollowerBot):
+    while robot.get_line_sensors() == [1024, 1024, 1024, 1024, 1024, 1024]:
+        robot.set_wheels_speed(100)
+        robot.sleep(0.01)
+        robot.set_wheels_speed(0)
+    print(robot.get_position())
+    robot.set_wheels_speed(25)
+    robot.sleep(1)
+    print(robot.get_position())
+    robot.done()
+
+
 if __name__ == '__main__':
-    robo = FollowerBot()
-    test_run(robo)
+    robo = FollowerBot(start_x=250, start_y=250)
+    drive_to_line(robo)
