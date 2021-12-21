80a3402af54f796384289809141c468a867c3c61
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-12-21 11:39:22 +0200

    robot

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -93,74 +93,21 @@ def the_true_follower(robot: FollowerBot):
         robot.set_wheels_speed(100)
         robot.sleep(0.01)
         robot.set_wheels_speed(0)
-    left_on_line = robot.get_left_line_sensors().count(0)
-    right_on_line = robot.get_right_line_sensors().count(0)
     while 0 in robot.get_line_sensors():
-        if robot.get_line_sensors().count(0) == 1:
-            if 0 in robot.get_left_line_sensors():
-                robot.set_left_wheel_speed(-100)
-                robot.set_right_wheel_speed(100)
-                robot.sleep(0.1407)
-                robot.set_wheels_speed(0)
-            if 0 in robot.get_left_line_sensors():
-                robot.set_left_wheel_speed(100)
-                robot.set_right_wheel_speed(-100)
-                robot.sleep(0.1407)
-                robot.set_wheels_speed(0)
-        elif left_on_line == right_on_line:
+        if robot.get_left_line_sensor() == 0 and robot.get_right_line_sensor() == 0:
             robot.set_wheels_speed(100)
             robot.sleep(0.01)
             robot.set_wheels_speed(0)
-            left_on_line = robot.get_left_line_sensors().count(0)
-            right_on_line = robot.get_right_line_sensors().count(0)
-        elif left_on_line == right_on_line + 1:
-            print("this triggers")
-            robot.set_left_wheel_speed(100)
-            robot.set_right_wheel_speed(50)
+        elif robot.time_from_start > 29:
+            break
+        elif robot.get_left_line_sensor() == 1024 and 0 in robot.get_right_line_sensors():
+            robot.set_left_wheel_speed(-100)
             robot.sleep(0.01)
             robot.set_wheels_speed(0)
-            left_on_line = robot.get_left_line_sensors().count(0)
-            right_on_line = robot.get_right_line_sensors().count(0)
-        elif left_on_line == right_on_line - 1:
-            print("this triggers 2")
-            robot.set_left_wheel_speed(50)
-            robot.set_right_wheel_speed(100)
-            robot.sleep(0.01)
-            robot.set_wheels_speed(0)
-            left_on_line = robot.get_left_line_sensors().count(0)
-            right_on_line = robot.get_right_line_sensors().count(0)
-        elif left_on_line == right_on_line + 2:
-            print("this triggers 3")
-            robot.set_left_wheel_speed(50)
-            robot.set_right_wheel_speed(10)
-            robot.sleep(0.01)
-            robot.set_wheels_speed(0)
-            left_on_line = robot.get_left_line_sensors().count(0)
-            right_on_line = robot.get_right_line_sensors().count(0)
-        elif left_on_line == right_on_line - 2:
-            print("this triggers 4")
-            robot.set_left_wheel_speed(10)
-            robot.set_right_wheel_speed(50)
-            robot.sleep(0.01)
-            robot.set_wheels_speed(0)
-            left_on_line = robot.get_left_line_sensors().count(0)
-            right_on_line = robot.get_right_line_sensors().count(0)
-        elif left_on_line == right_on_line + 3:
-            print("this triggers 5")
-            robot.set_left_wheel_speed(100)
+        elif robot.get_right_line_sensor() == 1024 and 0 in robot.get_left_line_sensors():
             robot.set_right_wheel_speed(-100)
             robot.sleep(0.01)
             robot.set_wheels_speed(0)
-            left_on_line = robot.get_left_line_sensors().count(0)
-            right_on_line = robot.get_right_line_sensors().count(0)
-        elif left_on_line == right_on_line - 3:
-            print("this triggers 6")
-            robot.set_left_wheel_speed(-100)
-            robot.set_right_wheel_speed(100)
-            robot.sleep(0.01)
-            robot.set_wheels_speed(0)
-            left_on_line = robot.get_left_line_sensors().count(0)
-            right_on_line = robot.get_right_line_sensors().count(0)
         else:
             break
     print(robot.get_line_sensors())
@@ -169,5 +116,5 @@ def the_true_follower(robot: FollowerBot):
 
 
 if __name__ == '__main__':
-    robo = FollowerBot(start_x=169, start_y=270, track_image="track3.png")
+    robo = FollowerBot(start_x=169, start_y=268, track_image="track3.png")
     the_true_follower(robo)
