adae8afa8c16c6dc48850ea6343c0f3dbf33c0a3
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-12-26 18:53:58 +0200

    test_exam

--- EXAM/exam0/exam.py
+++ EXAM/exam0/exam.py
@@ -9,5 +9,14 @@ def find_capital_letters(text: str) -> str:
     return capital_letters
 
 
+def close_far(a: int, b: int, c: int) -> bool:
+    if abs(abs(a) - abs(b)) <= 1 and abs(abs(a) - abs(c) >= 2):
+        return True
+    elif abs(abs(a) - abs(c)) <= 1 and abs(abs(a) - abs(b) >= 2):
+        return True
+    else:
+        return False
+
+
 if __name__ == "__main__":
     print(find_capital_letters("asdfsddddfvKJSHJDJKSKDkSKJDJKKJDSJFJKSJJdjsjdjsjejsjdjSjkDJsndJDKJjksd"))
