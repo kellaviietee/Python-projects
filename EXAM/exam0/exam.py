3da49e50863f19790d6b1ea68abbdce5e8cdf979
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2021-12-30 16:26:00 +0200

    test_exam

--- EXAM/exam0/exam.py
+++ EXAM/exam0/exam.py
@@ -201,8 +201,11 @@ def create_student(name: str, grades: list, credit_points: int) -> Student:
     Round the average grade up to three decimal places.
     If the list of grades is empty, the average grade will be 0.
     """
-    average_grade = round(sum(grades) / len(grades), 3)
-    return Student(name, average_grade, credit_points)
+    if len(grades) == 0:
+        return Student(name, 0, credit_points)
+    else:
+        average_grade = round(sum(grades) / len(grades), 3)
+        return Student(name, average_grade, credit_points)
 
 
 def get_top_student_with_credit_points(students: list, min_credit_points: int):
